dea1f3de781ebceb5ad60e793da7cc239f814d49
axs970 <2537004139@qq.com>
2017-05-09 18:51:34 +0800

    添加登录页，banner图

--- src/js/header.js
+++
@@ -1,8 +0,0 @@
-var $mune= $('.mune');
-var $title1 = $mune.find('li');
-var $content1 = $mune.find('div');
-$title1.on('mouseover',function(){
-	$(this).addClass('active').siblings().removeClass('active');
-	$content1.children().hide().eq(idx).slideDown();
-})
-console.log(666)
